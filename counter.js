fbcf4ccef3c35ce1cd5dcf33006d55e76e0f3903
nanpetchdeev <85831452+nanpetchdeev@users.noreply.github.com>
2021-08-03 15:39:01 +1200

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,19 @@
+let increment = document.querySelector("#increment");
+let decrement = document.querySelector("#decrement");
+let counterValue = document.querySelector("#counterValue");
+document.querySelector("#counterReset");
+
+var counter = 0;
+
+document.addEventListener("keydown", (e) => {
+	if(e.keyCode === 32) {
+		counter++
+		counterValue.innerText = counter
+	}
+	else if(e.keyCode === 8) {
+		if(counter > 0) {
+			counter--
+			counterValue.innerText = counter
+		}
+	}
+})
